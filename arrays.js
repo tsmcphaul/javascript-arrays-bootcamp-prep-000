b20cb9eb1ccbef8162b01b6f116169fcc04e8926
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-24 02:50:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,3 +41,7 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   return chocolateBars;
 }
 
+function removeElementFromEndOfArray(chocolateBars){
+  chocolateBars.slice(0, chocolateBars.length - 1);
+  return chocolateBars;
+}
